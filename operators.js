212f77b41d32877e5acf965dcfe94f3bc6bdd003
HARI_!! <harishanmugam62@gmail.com>
2026-06-12 11:23:58 +0530

    assignment

---
+++ operators.js
@@ -0,0 +1,17 @@
+//arithmetic
+var a=15
+var b=10
+console.log(a-b);//subtrscton
+console.log(a*b);//multiplication
+console.log(a/b);//division
+console.log(a%b);//modulus
+console.log(a**b);//exponentiation
+
+//assignment
+console.log("assignment operators")
+var c=40
+console.log(c=10)
+console.log(c*=18)
+console.log(c/=10)
+console.log(c%=10)
+console.log(c**=10)
---
+++ variable.js
@@ -0,0 +1,12 @@
+//using variable decleration with var
+var name = "hari"
+console.log(name)
+
+//using variable decleration with let
+let number = 12
+console.log(number)
+
+//using variable decleration with const
+const country = "india"
+console.log(country)
+
